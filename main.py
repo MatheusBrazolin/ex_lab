a42be66904e32e3f6738231f41655ca32f4d1373
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-11 21:24:56 -0300

    Att lab

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def read_root():
+    return {"Hello World! from": "Matheus"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
